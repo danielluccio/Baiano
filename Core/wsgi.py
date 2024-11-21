18e4b9a2072e81bf7cacd1b0ae75df67bac49df0
danielluccio <daniel130877@gmail.com>
2024-11-21 20:20:28 -0300

    deploy-changes

--- Core/wsgi.py
+++ Core/wsgi.py
@@ -1,3 +1,11 @@
+"""
+WSGI config for Core project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
 
 import os
 
@@ -6,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Core.settings")
 
 application = get_wsgi_application()
-
-app = application
